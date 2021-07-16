80f910b7073a58feae43c399debcd6bd6dc9b09f
jjomonero <https://github.com/jjomonero>
2021-07-16 17:48:34 -0300

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        Climatizadores Perferro
+      </h1>
     </div>
   );
 }
